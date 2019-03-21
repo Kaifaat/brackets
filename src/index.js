135060134c90cf64a9472cf98573d0fc895dd0e6
Kaifaat <artem.meka@gmail.com>
2019-03-21 19:21:59 +0300

    feat: add brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let bracketsArray = [];
+    str.split('').forEach(function(item, i) {
+        if (i == 0) {
+            bracketsArray.push(item);
+            return;
+        }
+        let lastOpenedBracket = bracketsConfig.find(bracketType =>
+            bracketsArray[bracketsArray.length - 1] == bracketType[0]);
+        lastOpenedBracket = lastOpenedBracket && lastOpenedBracket[1];
+        if (item != lastOpenedBracket) {
+            bracketsArray.push(item);
+        }
+        else {
+            bracketsArray.pop();
+        }
+    });
+    if (bracketsArray.length == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
 }
